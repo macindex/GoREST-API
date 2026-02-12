68b6f601fd079d3818127647bfa24519153e6b6b
macindex <machollanda@gmail.com>
2026-02-12 20:31:04 -0300

    handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,3 +1,18 @@
 package handlers
 
 
+type handlers struct {
+	useCases useCases.useCases
+}
+
+func New(useCases *usecases.UseCases) *Handlers {
+	return &Handlers()
+}
+
+func (h Handlers) Listen(port int) error {
+	h.registerUserEndpoints()
+	return http.ListenAndServer(
+		fmt.Printf(":%v", port),
+		nil,
+	)
+}
