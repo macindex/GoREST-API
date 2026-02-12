9a4b3f279b8b933dc3c09bcefca0d72df25f0cbc
macindex <machollanda@gmail.com>
2026-02-11 23:04:27 -0300

    users

--- internal/models/users.go
+++ internal/models/users.go
@@ -16,6 +16,15 @@ func (u Users) GetAll() []models.User {
 	return u.users
 }
 
+func (u Users) EmailExists() (bool) {
+	for _, v range u.users {
+		if v.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 func (u *Users) Add(newUser models.User) {
 	u.users = append(u.users, newUser)
 }
